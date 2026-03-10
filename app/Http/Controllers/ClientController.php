a91e3d6af54614b3c53e57cdc07dfaaab6d8ef2e
Reload-Lab <info@reloadlab.it>
2026-03-10 15:06:45 +0100

    Mostrato elenco clienti da database

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
     public function index()
     {
-        return view('clients.index');
+        $clients = Client::all();
+
+        return view('clients.index', compact('clients'));
     }
 }
--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -4,6 +4,16 @@
 
 <h2>Clienti</h2>
 
-<p>Elenco clienti</p>
+@if($clients->count())
+    <ul>
+        @foreach($clients as $client)
+            <li>
+                {{ $client->name }} - {{ $client->email }} - {{ $client->phone }}
+            </li>
+        @endforeach
+    </ul>
+@else
+    <p>Nessun cliente presente</p>
+@endif
 
 @endsection
