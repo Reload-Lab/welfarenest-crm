e53f5970860d0f4750b09a00eeeeb64e98aaec9b
Alessio Attanasio <info@reloadlab.it>
2026-06-17 15:24:07 +0200

    Exclude OIDC token endpoint from CSRF

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'wn-plus/oidc/token',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
